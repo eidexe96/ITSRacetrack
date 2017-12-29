2480eb74dcb0ec19d4af5ff1c11ac8b9ba4822df
Fynn <fynnkonig@gmail.com>
2017-12-29 23:36:18 +0100

    showResults
    Created function showResults and prepared webserver. Main Loop needs to be adjusted. Known Issues:
    -Threading needs to be reduced
    -Show Results and Webserver need to be properly integrated

--- showResults.py
+++ showResults.py
@@ -1,2 +1,46 @@
-def showResults(checkpoint, teamid):
-    x = 7
+import pandas as pd
+import time
+import math
+
+def showResults(activeCheckpoint, teamid, starttime):
+    fn = "test.csv" #CSV - Datenbank
+    htmlpage = "test.html" #Zieldatei html
+    if activeCheckpoint == 0:
+        #show Team info
+        df = pd.DataFrame.from_csv(fn)
+        table = pd.DataFrame.to_html(df)
+        htmlOut= open(htmlpage,"w")
+        html = '<html><head><title>Racing Info</title><meta http-equiv="refresh" content="5"></head><body><p>Racing Infos here</p><a href="http://localhost:8081/0">Anderer Text</a>' 
+        html += table
+        html +='<p>presented by Dem Boiz</p></body></html>' 
+        htmlOut.write(html)
+        htmlOut.close()
+    elif activeCheckpoint > 0 and activeCheckpoint < 9:
+        #show Live Time
+        dt = time.time() - starttime
+        livetime = str(math.floor(dt/60))
+        livetime += " Minuten "
+        livetime += str(round(dt%60))
+        livetime += " Sekunden"
+        htmlOut= open(htmlpage,"w")
+        html = '<html><head><title>Racing Info</title><meta http-equiv="refresh" content="5"></head><body><p>Racing Infos here</p><a href="http://localhost:8081/0">Anderer Text</a>' 
+        html += livetime      #aktuell gefahrene Zeit seit Start
+        html +='<p>presented by Dem Boiz</p></body></html>' 
+        htmlOut.write(html)
+        htmlOut.close()
+    elif activeCheckpoint == 9:
+        #show Results after Race End
+        htmlOut= open(htmlpage,"w")
+        html = '<html><head><title>Racing Info</title><meta http-equiv="refresh" content="5"></head><body><p>Racing Infos here</p><a href="http://localhost:8081/0">Anderer Text</a>' 
+        html += table
+        html +='<p>presented by Dem Boiz</p></body></html>' 
+        htmlOut.write(html)
+        htmlOut.close()
+    else:
+        #show Error
+        htmlOut= open(htmlpage,"w")
+        html = '<html><head><title>Racing Info</title><meta http-equiv="refresh" content="5"></head><body><p>Racing Infos here</p><a href="http://localhost:8081/0">Anderer Text</a>' 
+        html += "An Error occured. Please contact your beloved RaceTrack Team!"
+        html +='<p>Thank you.</p></body></html>' 
+        htmlOut.write(html)
+        htmlOut.close()
