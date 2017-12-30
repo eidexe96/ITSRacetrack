a981d3926b7b34098de972c60d5dd9cd3203543d
Fynn <fynnkonig@gmail.com>
2017-12-30 19:04:43 +0100

    MainLoop & Server
    embedded showResults, QR Code, Server and some small Changes. Simplified Threading. Established playSartSequence as function from light.py

--- mainLoop.py
+++ mainLoop.py
@@ -1,37 +1,39 @@
-import time
+﻿import time
 import _thread
 import checkpointLogic as cpl
 import sound
 import light as lit
 import showResults as rslt
 import buttonPressed as btn
+import runServer
+import readQRCode as qr
+_thread.start_new_thread(runServer.startServer, ())
+
+global abort
+abort = False
 
 def startProgramm():
     identified = False
     while identified == False:                                   #Loop solange kein Auto identifiziert ist
-        #Read QR Code
-        #get Variables
-        #such as teamid & identified
+        teamid, identified = qr.readQRCode()                     #Wenn der Scan funktioniert hat, wird die teamid und ein True-Flag zurückgegeben
         if identified == True:                                   #Initialisiere Startsequenz sobald ein Auto erkannt wurde
-            rslt.showResults(0, teamid)                               #zeigt bisherige Ergebnisse des Teams an
+            rslt.showResults(0, teamid, 0)                       #zeigt bisherige Ergebnisse des Teams an
             sound.playSpeech(0, teamid)                          #erzählt infos zum Team
-            _thread.start_new_thread( sound.playSound, (0) )     #spielt Startsounds ab und zeitgleich:
-            _thread.start_new_thread( lit.startLightExpress, (0) )   #startet Ampelsequenz zum Start des Rennens
+            lit.playStartSequence()                              #startet Ampel, Sounds und Fehlstartkontrolle
     
     starttime = clock()                                          #startet die Zeitmessung unabhängig davon, ob das Auto zu spät los fährt
     activeCheckpoint = 1
-    abort=False
     
     while identified == True:                                                   #Loop für Rennen, Messung etc.
         if checkpointReached(activeCheckpoint % 4):
-            saveTime(starttime, activeCheckpoint % 4)                       #speichert die LiveZeit vom aktuellen Checkpoint [0,1,2,3]
+            cpl.saveTime(starttime, activeCheckpoint % 4)                       #speichert die LiveZeit vom aktuellen Checkpoint [0,1,2,3]
             _thread.start_new_thread(lit.startLightExpress,(activeCheckpoint))  #Ampel leuchtet, checkpointabhängig, läuft parallel
             _thread.start_new_thread(sound.playSound,(activeCheckpoint))        #Spielt parallel den Sound, checkpointabhängig [1...8]
             _thread.start_new_thread(sound.playSpeech,(activeCheckpoint, teamid)) #spricht (parallel),checkpointabhängig [1...8],teamid?
-            rslt.showResults(activeCheckpoint, teamid)                               #Zeigt Ergebnisse
+            rslt.showResults(activeCheckpoint, teamid, starttime)                #Zeigt Ergebnisse
             activeCheckpoint += 1
         if activeCheckpoint == 9:                                               #beendet das Rennen, speichert und zeigt Ergebnisse
-            prepareDataForDB(teamID)                                        #rechnet einzelne Zeiten aus und schreibt diese in die DB
+            cpl.prepareDataForDB(teamID)                                        #rechnet einzelne Zeiten aus und schreibt diese in die DB
             rslt.showResults(9, teamid)
             break
         
--- runServer.py
+++ runServer.py
@@ -29,16 +29,17 @@ class MyHandler(BaseHTTPRequestHandler):
         response = self.handle_http(opts['status'], self.path)
         self.wfile.write(response)
 
-if __name__ == '__main__':
-    server_class = HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
+def startServer():
+    if __name__ == '__main__':
+        server_class = HTTPServer
+        httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+        print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
 
-    try:
-        httpd.serve_forever()
-        time.sleep(10)
+        try:
+            httpd.serve_forever()
+            time.sleep(10)
+            httpd.server_close()
+        except KeyboardInterrupt:
+            pass
         httpd.server_close()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
+        print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
