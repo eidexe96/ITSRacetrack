6a57881469e605040c7f439b60bdfbb0834d3e46
reggie1408 <32675258+reggie1408@users.noreply.github.com>
2018-01-05 16:53:18 +0100

    sound eingefügt

--- sound.py
+++ sound.py
@@ -1,4 +1,71 @@
-def playSound(checkpoint):
-    x = 7
-def playSpeech(checkpoint, teamid):
-    x = 8
+from subprocess import call
+from random import randint
+
+#speech synthesis functions
+
+def playSpeech(check, teamID):
+    
+    if check == 1 or check == 2 or check == 3 or check == 5 or check == 6 or check == 7:
+        z = randint(1,10)
+        if z == 1:
+            text = "Beispiel 1"
+        elif z == 2:
+            text = "Beispiel 2"
+        elif z == 3:
+            text = "Beispiel 3"
+        elif z > 5:
+            text = ""
+            
+    elif check == 4:
+        z = randint(1,5)
+        if z == 1:
+            text = "Beispiel 1"
+        elif z == 2:
+            text = "Beispiel 2"
+        elif z == 3:
+            text = "Beispiel 3"
+            
+    elif check == 8:
+        z = randint(1,5)
+        if z == 1:
+            text = "Beispiel 1"
+        elif z == 2:
+            text = "Beispiel 2"
+        elif z == 3:
+            text = "Beispiel 3"
+    
+    call(["espeak", "-vmb-de4", text, "2>/dev/null"])
+
+def s_speech(var2):
+    call(["pico2wave", "-lang=de-DE", "-wave=/home/pi/work/sound/test.wav", var2])
+    call(["aplay", "/home/pi/work/sound/test.wav"])
+    call(["rm", "/home/pi/work/sound/test.wav"])
+    
+    
+    
+#sound function
+
+def playSound(check):
+    
+    if check == -3:
+        sound = "scan.wav" 
+    elif check == -2:
+        sound = "start.wav"
+    elif check == -1:
+        sound = "beep_1.wav"
+    elif check == 0:
+        sound = "beep_2.wav"
+    elif check == 1 or check == 2 or check == 3 or check == 5 or check == 6 or check == 7:
+        sound = "check.wav"
+    elif check == 4:
+        sound = "2nd_lap.wav"
+    elif check == 8:
+        sound = "end_good.wav"
+    
+    call(["aplay", "/home/pi/work/sound/"+sound])
+        
+    
+    
+    
+
+
